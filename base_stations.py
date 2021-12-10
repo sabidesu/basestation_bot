61ed5c006f92cc2a511f701d762647e313f84622
sabidesu <zentixzaneic@gmail.com>
2021-12-09 23:48:12 -0500

    improve line readability

--- base_stations.py
+++ base_stations.py
@@ -4,20 +4,22 @@ import urllib.request # used to download website
 import re # used to find necessary part of website
 import credentials as creds # contains twitter dev acct creds
 
+# create twitter client
+acct = tweepy.Client(creds.BEARER_TOKEN, creds.CONSUMER_KEY, \
+	creds.CONSUMER_SECRET, creds.ACCESS_KEY, creds.ACCESS_SECRET)
+
 # parse website for information
 url = "https://store.steampowered.com/valveindex"
 s = urllib.request.urlopen(url)
-text = s.read().decode("utf-8").replace("\r\n", "").replace("\t", "").replace(" ", "")
-regex = r"\$149.00<\/div><divclass=\"btn_addtocart\"><spanclass=\".*\"><span>(.*)<\/span>(<\/div>){4,}"
+text = s.read().decode("utf-8").replace("\r\n", "").replace("\t", "") \
+	.replace(" ", "")
+regex = r"\$149.00<\/div><divclass=\"btn_addtocart\"><spanclass=\".*\">" \
+	+ "<span>(.*)<\/span>(<\/div>){4,}"
 finder = re.compile(regex)
 
-for match in finder.finditer(text):
-	print(match.group(1))
-
-# acct = tweepy.Client(creds.BEARER_TOKEN, creds.CONSUMER_KEY, creds.CONSUMER_SECRET, creds.ACCESS_KEY, creds.ACCESS_SECRET)
-
-# attempt to tweet
-# try:
-# 	print(acct.create_tweet(text="test tweet #1"))
-# except tweepy.errors.Forbidden:
-# 	print("tweet failed to send")
+# generate tweet based on information
+status = "no" if finder.search(text).group(1) == "OutofStock" else "YES!"
+try:
+	acct.create_tweet(text="no")
+except tweepy.errors.Forbidden:
+	acct.create_tweet(text="status unknown")
