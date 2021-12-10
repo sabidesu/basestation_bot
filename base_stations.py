e97cb450d2a81cbc1f38895f7512e6fb2aeb3b35
sabidesu <zentixzaneic@gmail.com>
2021-12-09 20:43:28 -0500

    get tweeting working

--- base_stations.py
+++ base_stations.py
@@ -6,7 +6,10 @@ CONSUMER_KEY = "Ph2AZPPaiz37s2zZINPH8SWt6"
 CONSUMER_SECRET = "GHdlpOjp2dZ7qAu7GTgQ3gLokXinAUsd6z7NblbGDBLSSQQHcX"
 ACCESS_KEY = "1469102662775971846-T2U0IjlcAyQHH7d2zr9o3ol1lvPwsj"
 ACCESS_SECRET = "VlAKs962TaoYaSE0WRcHJwp9Vsfwydq0MkIJ937MtZrJ2"
+BEARER_TOKEN = "AAAAAAAAAAAAAAAAAAAAALk0WwEAAAAALMFjf3MRE1S7xnsGFcv1bLtS1V0%3DPsaybQk1mZrrJP21zYuGoUYzWGDRqVJmnk23ESpO9ta5bPhovL"
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
+acct = tweepy.Client(BEARER_TOKEN, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+try:
+	print(acct.create_tweet(text="another test tweet"))
+except tweepy.errors.Forbidden:
+	print("tweet failed to send")
